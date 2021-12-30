3c685fc36a3dd9eb198fb22dbfa10fefe68e973e
TomSoulage <soulagetom@eisti.eu>
2021-12-30 19:14:54 +0100

    ajout header et footer

--- projet-cookie/src/app/app-routing.module.ts
+++ projet-cookie/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'test', component:AppComponent},
+  {path:'accueil',component:AccueilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projet-cookie/src/app/app.module.ts
+++ projet-cookie/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { HeaderComponent } from './structure/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './structure/footer/footer.component'; 
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button'; 
+
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
